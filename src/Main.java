5b0c7c661820a0800f2521ad039f77faafa99de5
Бексултан <nightwing_14@mail.ru>
2026-04-01 14:55:11 +0300

    Start 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     int firstFriday = 3;
     for (int i = 0; i <= 31; i++) {
         if ((i - firstFriday) % 7 == 0) {
-            System.out.println("Сегодня пятница, "+ i +"-е число. Необходимо подготовить отчет");
+            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет");
         }
     }
+    int distanceCovered = 0;
+    int finalDistantion = 42195;
+    int checkingTheDistance = 500;
+    do{distanceCovered = distanceCovered + checkingTheDistance;
+        System.out.println("Держитесь! Осталось " + distanceCovered+ " метров");
+    }while(distanceCovered <= finalDistantion);
+
+    int distanceCoveredTwo = 0;
+    int finalDistantionTwo = 42195;
+    int checkingTheDistanceTwo = 500;
+    for(; distanceCoveredTwo <= finalDistantionTwo; ) {
+            distanceCoveredTwo = distanceCoveredTwo + checkingTheDistanceTwo;
+            System.out.println("2.0 Держитесь! Осталось " + distanceCoveredTwo+ " метров");
+    }
 }
+
