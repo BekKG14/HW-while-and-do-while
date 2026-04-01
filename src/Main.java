a0439b1e223f21b06f494aa1ac30df08aa57f07c
Бексултан <nightwing_14@mail.ru>
2026-04-01 11:09:51 +0300

    Start

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-
+    int firstFriday = 3;
+    for (int i = 0; i <= 31; i++) {
+        if ((i - firstFriday) % 7 == 0) {
+            System.out.println("Сегодня пятница, "+ i +"-е число. Необходимо подготовить отчет");
+        }
     }
 }
